f62a19f1b7c26a567a652af2bd0e2f61ef8bf2fe
sakshimulekar <sakshimulekar2017@gmail.com>
2023-03-29 13:14:46 +0530

    newone

--- project/src/component/cart/Cart.jsx
+++ project/src/component/cart/Cart.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import logo from "../images/StyleHub.png"
 
 export const Cart = () => {
   return (
     <div>
         <div className="nav">
             <div className="logo">
-                <img src="./StyleHub.png" alt="logo" />
+                <img src={logo} alt="logo" />
             </div>
 
         </div>
