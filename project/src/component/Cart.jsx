d7d1ec3e0b8aedf1d68fec8942249209eba4fdc1
sakshimulekar <sakshimulekar2017@gmail.com>
2023-03-29 13:01:00 +0530

    sak_dummy

---
+++ project/src/component/Cart.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Cart = () => {
+  return (
+    <div>
+        <div className="nav">
+            <div className="logo">
+                <img src="./StyleHub.png" alt="logo" />
+            </div>
+
+        </div>
+    </div>
+  )
+}
