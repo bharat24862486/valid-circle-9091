d90f251547cc97d1197b2e91e883181c3568c8f4
Rahul Singh <rahul1905.sk@gmail.com>
2023-03-31 04:31:37 +0530

    testing 2

--- project/src/component/ProductPage/ProductPage.jsx
+++ project/src/component/ProductPage/ProductPage.jsx
@@ -376,6 +376,7 @@ const ProductPage = () => {
                 borderRadius={"12"}
                 boxShadow=" rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;"
               >
+                
                 <Menu isOpen={isOpenMenu}>
                   <MenuButton
                     as={Button}
