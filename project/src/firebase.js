36c884e80aeac751bb9736335bf3f6776d2017b2
Bharatsharma <108224876+bharat24862486@users.noreply.github.com>
2023-08-08 11:53:57 +0530

    ok

--- project/src/firebase.js
+++ project/src/firebase.js
@@ -1,23 +1,25 @@
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
+  apiKey: "AIzaSyDL2xy2wlQ2c1akSAeYhYEG0dXThCXJzms",
+  authDomain: "loginotp-611df.firebaseapp.com",
+  projectId: "loginotp-611df",
+  storageBucket: "loginotp-611df.appspot.com",
+  messagingSenderId: "986918936503",
+  appId: "1:986918936503:web:89c36987fc45affe905232",
+  measurementId: "G-XNX5NSG205"
+
+  // apiKey: "AIzaSyBvhjsopUc4mKo6fmxO9o1jxTZiOep7GPk",
+  // authDomain: "okotp-808e3.firebaseapp.com",
+  // projectId: "okotp-808e3",
+  // storageBucket: "okotp-808e3.appspot.com",
+  // messagingSenderId: "367684623113",
+  // appId: "1:367684623113:web:70900f48970b0e4a3711d8",
+  // measurementId: "G-3B4MZGHBNH"
 
 
-    //   apiKey: "AIzaSyCKAHylaBbfbdIOzDxQR558w6m5P-iAZXk",
-    //   authDomain: "first-backend-c79e1.firebaseapp.com",
-    //   projectId: "first-backend-c79e1",
-    //   storageBucket: "first-backend-c79e1.appspot.com",
-    //   messagingSenderId: "645329550531",
-    //   appId: "1:645329550531:web:c3e9fbe91a944368d8b676",
-    //   measurementId: "G-0WER38B2BX"
-    apiKey: "AIzaSyCKAHylaBbfbdIOzDxQR558w6m5P-iAZXk",
-    authDomain: "first-backend-c79e1.firebaseapp.com",
-    projectId: "first-backend-c79e1",
-    storageBucket: "first-backend-c79e1.appspot.com",
-    messagingSenderId: "645329550531",
-    appId: "1:645329550531:web:3f67c5d61dc904e5d8b676",
-    measurementId: "G-G6YCEG34H5"
 
 
 };
@@ -25,4 +27,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app)
+export const auth=getAuth(app)
